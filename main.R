5c8fd2ee5181b4cd729cee69df47b8efcc7cd146
Owen Sims <owen.sims@cyberlytic.com>
2018-07-23 15:35:52 +0100

    Add a main R function to generate the images

--- main.R
+++ main.R
@@ -11,6 +11,8 @@ library(hrbrthemes)
 
 
 # Source all R functions required into the Global Environment
+print("INFO: Sourcing requred functions into the current workspace...")
+
 RFunctionFiles <- getwd() %>% 
   paste0("/R/") %>% 
   list.files(
@@ -20,11 +22,21 @@ for (file in RFunctionFiles) getwd() %>%
   paste0("/R/", file) %>% 
   source()
 
+print("SUCCESS: Successfully sourced all functions from the `R` directory!")
+
 # Collect the geographical data
+print("INFO: Collecting geographical data...")
+
 geoData <- getGeoData()
 
+print("SUCCESS: Successfully collected all geographical data!")
+
 # Generate the European patent map
+print("INFO: Generating the European patent map...")
+
 patentMap <- generateMap(
   neigh = geoData$neigh, 
   bord = geoData$bord, 
   gd3 = geoData$gd3)
+
+print("SUCCESS: Successfully generated the European patent map and saved to the `images` directory!")
