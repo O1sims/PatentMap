1e3623c5957420113a3ca96849d02ab402b7756c
Owen Sims <owen.sims@cyberlytic.com>
2018-07-23 14:19:15 +0100

    Tidy up code indenting; minor changes

--- R/getStatData.R
+++ R/getStatData.R
@@ -1,16 +1,17 @@
-
-getStatData <- function() {
-        both15 <- eurostat::euroget_eurostat("demo_r_pjanaggr3") %>% 
-                filter(sex == "T", nchar(paste(geo)) == 5,
-                       !str_sub(geo, 1, 4) %in% remote,
-                       !str_sub(geo, 1, 2) %in% c("AL", "MK"),
-                       year(time) == 2015) %>% 
-                droplevels() %>% 
-                transmute(id = geo, age, value = values) %>% 
-                spread(age, value) %>% 
-                mutate(sy = Y_LT15 / TOTAL, 
-                       sw = `Y15-64` / TOTAL, 
-                       so = 1 - (sy + sw))
-        
-        return(both15)
+getStatData <- function(remote) {
+  both15 <- eurostat::euroget_eurostat("demo_r_pjanaggr3") %>% 
+    filter(
+      sex == "T",
+      nchar(paste(geo)) == 5,
+      !str_sub(geo, 1, 4) %in% remote,
+      !str_sub(geo, 1, 2) %in% c("AL", "MK"),
+      year(time) == 2015) %>% 
+    droplevels() %>% 
+    transmute(id = geo, age, value = values) %>% 
+    spread(age, value) %>% 
+    mutate(sy = Y_LT15 / TOTAL, 
+           sw = `Y15-64` / TOTAL, 
+           so = 1 - (sy + sw))
+  
+  return(both15)
 }
--- R/ggHole.R
+++ R/ggHole.R
@@ -2,10 +2,10 @@
 # See: https://stackoverflow.com/questions/21748852
 
 ggHole <- function(fort) {
-        poly <- fort[fort$id %in% fort[fort$hole,]$id,]
-        hole <- fort[!fort$id %in% fort[fort$hole,]$id,]
-        out <- list(poly,hole)
-        names(out) <- c('poly','hole')
-        return(out)
+  poly <- fort[fort$id %in% fort[fort$hole,]$id, ]
+  hole <- fort[!fort$id %in% fort[fort$hole,]$id, ]
+  out <- list(poly, hole)
+  names(out) <- c('poly', 'hole')
+  return(out)
 }
 
