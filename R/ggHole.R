6039863b51bc03f18d5146aa936abad335b0db36
Owen Sims <owen.sims@cyberlytic.com>
2018-07-23 13:57:54 +0100

    Add initial helper functions

---
+++ R/ggHole.R
@@ -0,0 +1,11 @@
+# A function to overcome the problem of mapping nested polygons
+# See: https://stackoverflow.com/questions/21748852
+
+ggHole <- function(fort) {
+        poly <- fort[fort$id %in% fort[fort$hole,]$id,]
+        hole <- fort[!fort$id %in% fort[fort$hole,]$id,]
+        out <- list(poly,hole)
+        names(out) <- c('poly','hole')
+        return(out)
+}
+
---
+++ R/identifyBorders.R
@@ -0,0 +1,14 @@
+identifyBorders <- function(SPolyDF) {
+        borders <- rgeos::gDifference(
+                as(SPolyDF, "SpatialLines"),
+                as(gUnaryUnion(SPolyDF), "SpatialLines"),
+                byid = TRUE)
+        
+        df <- data.frame(
+                len = sapply(1:length(borders),
+                             function(i) gLength(borders[i, ])))
+        rownames(df) <- sapply(1:length(borders),
+                               function(i) borders@lines[[i]]@ID)
+        SLDF <- sp::SpatialLinesDataFrame(borders, data = df)
+        return(SLDF)
+}
